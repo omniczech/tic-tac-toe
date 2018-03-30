f2ed0c6f4c707b3b7b0323e77314836be04889ad
omniczech <mikepetschek@gmail.com>
2018-03-30 10:49:37 -0400

    Refined Game logic
    Game can now detect the 8 game winning plays. if one is met, console log
    displays who won. If all spaces are taken but no one has won, logs that
    it's a draw. Clicking a space now removes its on click function. The
    game ending similarly removes the ability to trigger the on click
    function as well. Added a restart function, currently resets game
    variables.
    
    Next Steps: rebind click functions on restart.
    Add warning when user tries to click already clicked space.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,18 +1,51 @@
 
 let turnOrder = 1
+const gameBoardArray = ['', '', '', '', '', '', '', '', '']
 
 const addLetter = (e) => {
+  const arrayPos = $(e.target).attr('data-array-order')
+  let currentLetter
   if (turnOrder % 2 === 1) {
-    $(e.target).append('<p>X</p>')
+    currentLetter = 'x'
   } else {
-    $(e.target).append('<p>O</p>')
+    currentLetter = 'o'
   }
-  $(e.target).removeClass('open')
+  gameBoardArray[arrayPos] = currentLetter
+  $(e.target).append(`<h1>${currentLetter}</h1>`)
+  $(e.target).off('click')
+  if (
+    (gameBoardArray[0] !== '' && gameBoardArray[0] === gameBoardArray[1] && gameBoardArray[1] === gameBoardArray[2]) ||
+    (gameBoardArray[3] !== '' && gameBoardArray[3] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[5]) ||
+    (gameBoardArray[6] !== '' && gameBoardArray[6] === gameBoardArray[7] && gameBoardArray[7] === gameBoardArray[8]) ||
+    (gameBoardArray[0] !== '' && gameBoardArray[0] === gameBoardArray[3] && gameBoardArray[3] === gameBoardArray[6]) ||
+    (gameBoardArray[1] !== '' && gameBoardArray[1] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[7]) ||
+    (gameBoardArray[2] !== '' && gameBoardArray[2] === gameBoardArray[5] && gameBoardArray[5] === gameBoardArray[8]) ||
+    (gameBoardArray[0] !== '' && gameBoardArray[0] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[8]) ||
+    (gameBoardArray[6] !== '' && gameBoardArray[6] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[2])
+
+  ) {
+    $('.game-space').off('click')
+    console.log(`${gameBoardArray[0].toUpperCase()} Wins!`)
+  } else if (turnOrder === 9) {
+    console.log('It\'s a draw')
+  }
+  // $(e.target).removeClass('open')
+  console.log(gameBoardArray)
   turnOrder++
+}
+
+const newGame = () => {
+  $('.game-space').html('')
+  turnOrder = 1
+  gameBoardArray.forEach(space => {
+    space = ''
+    return 'false'
+  })
 }
 
 const addHandlers = () => {
   $('.game-space.open').on('click', addLetter)
+  $('.new-game').on('click', newGame)
 }
 
 module.exports = {
